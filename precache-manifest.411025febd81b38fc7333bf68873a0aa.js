aa490772bb66cdb0316e1780294188ac4a8bc011
vikram kumar <vikramkumaralpha@gmail.com>
2021-09-22 18:40:36 +0530

    Updates

---
+++ precache-manifest.411025febd81b38fc7333bf68873a0aa.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0906088a4d9637911aa44d7cb33c7d",
+    "url": "/wanderlustclientside/index.html"
+  },
+  {
+    "revision": "618171d17152757ae275",
+    "url": "/wanderlustclientside/static/css/2.c520df0e.chunk.css"
+  },
+  {
+    "revision": "ac625c38118e9311e387",
+    "url": "/wanderlustclientside/static/css/main.850f0a81.chunk.css"
+  },
+  {
+    "revision": "618171d17152757ae275",
+    "url": "/wanderlustclientside/static/js/2.9ba16d3d.chunk.js"
+  },
+  {
+    "revision": "5f881245bbdb825ad69762e741b0c0ef",
+    "url": "/wanderlustclientside/static/js/2.9ba16d3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac625c38118e9311e387",
+    "url": "/wanderlustclientside/static/js/main.54240d58.chunk.js"
+  },
+  {
+    "revision": "639a2b2e693c33a74376",
+    "url": "/wanderlustclientside/static/js/runtime-main.a881905d.js"
+  },
+  {
+    "revision": "ecca46c84baa9108093880c48865b8a0",
+    "url": "/wanderlustclientside/static/media/bg-masthead1.ecca46c8.jpg"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/wanderlustclientside/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/wanderlustclientside/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/wanderlustclientside/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "2d2afb2719a1ee903e576e7c457daf81",
+    "url": "/wanderlustclientside/static/media/primeicons.2d2afb27.eot"
+  },
+  {
+    "revision": "66ee0deb739ca71f0ecdc39d7c1b22cb",
+    "url": "/wanderlustclientside/static/media/primeicons.66ee0deb.woff"
+  },
+  {
+    "revision": "df0140f8e79ecfeffaf85220aaecd7c4",
+    "url": "/wanderlustclientside/static/media/primeicons.df0140f8.ttf"
+  },
+  {
+    "revision": "e5e0e94474d5fd92e7e800a8865d297c",
+    "url": "/wanderlustclientside/static/media/primeicons.e5e0e944.svg"
+  }
+]);
